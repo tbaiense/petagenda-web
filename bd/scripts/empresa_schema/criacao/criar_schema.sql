ec5767bae613c209823ef24487d4b71d2c1845f9
tbaiense <tbaiense@proton.me>
2025-04-04 08:34:54 -0300

    insere coluna "id" na tabela de "reserva_funcionario" e atualiza dicionario, modelo logico e scripts

--- bd/scripts/empresa_schema/criacao/criar_schema.sql
+++ bd/scripts/empresa_schema/criacao/criar_schema.sql
@@ -17,14 +17,18 @@ CREATE TABLE funcionario (
 );
 
 CREATE TABLE reserva_funcionario(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     id_funcionario INT NOT NULL,
     data DATE NOT NULL,
     hora_inicio TIME NOT NULL,
     hora_fim TIME NOT NULL,
     
+    UNIQUE (id_funcionario, data, hora_inicio),
+    UNIQUE (id_funcionario, data, hora_fim),
     CONSTRAINT chk_reserva_funcionario_hora_inicio_AND_hora_fim CHECK (hora_inicio < hora_fim),
     FOREIGN KEY (id_funcionario) REFERENCES funcionario(id)
 );
+
 
 CREATE TABLE categoria_servico (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
