53e9e5c0373a2d5801c562f47f80aa2351683651
tbaiense <tbaiense@proton.me>
2025-04-11 16:30:55 -0300

    adiciona procedures, views, triggers e atualiza triggers de pet_servico, agendamento, e servico_realizado

--- bd/scripts/empresa_schema/criacao/triggers/create_trigger_pet_servico.sql
+++ bd/scripts/empresa_schema/criacao/triggers/create_trigger_pet_servico.sql
@@ -29,3 +29,107 @@ CREATE TRIGGER trg_pet_servico_insert /* Faz a atribuição do valor do preço d
         END IF;
     END;$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER trg_pet_servico_insert_after
+    AFTER INSERT
+    ON pet_servico
+    FOR EACH ROW
+    BEGIN
+        DECLARE id_agend INT;
+        DECLARE id_serv_real INT;
+        DECLARE NEW_valor_total DECIMAL(8,2);
+        DECLARE NEW_valor_servico DECIMAL(8,2);
+
+        -- Obtém valores para cobrança do agendamento ou servico_realizado
+        IF NEW.id_info_servico IS NOT NULL THEN
+            CALL get_valores_info_servico(NEW.id_info_servico, NEW_valor_servico, NEW_valor_total);
+
+            -- Obtendo id do servico_realizado
+            SELECT
+                id
+                INTO id_serv_real
+                FROM servico_realizado
+                WHERE
+                    id_info_servico = NEW.id_info_servico
+                LIMIT 1;
+
+            -- Atualizando valores no servico_realizado
+            IF id_serv_real IS NOT NULL THEN
+                UPDATE servico_realizado
+                    SET valor_servico = NEW_valor_servico,
+                        valor_total = NEW_valor_total
+                    WHERE id = id_serv_real;
+            END IF;
+
+            -- Obtendo o id do agendamento
+            SELECT
+                id
+                INTO id_agend
+                FROM agendamento
+                WHERE
+                    id_info_servico = NEW.id_info_servico
+                LIMIT 1;
+
+            -- Atualizando valores no agendamento
+            IF id_agend IS NOT NULL THEN
+                UPDATE agendamento
+                SET valor_servico = NEW_valor_servico,
+                    valor_total = NEW_valor_total
+                WHERE id = id_agend;
+            END IF;
+        END IF;
+    END;$$
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE TRIGGER trg_pet_servico_update
+    AFTER UPDATE
+    ON pet_servico
+    FOR EACH ROW
+    BEGIN
+        DECLARE id_agend INT;
+        DECLARE id_serv_real INT;
+        DECLARE NEW_valor_total DECIMAL(8,2);
+        DECLARE NEW_valor_servico DECIMAL(8,2);
+
+        -- Obtém valores atualizados para cobrança do agendamento ou servico_realizado
+        IF NEW.id_info_servico IS NOT NULL THEN
+            CALL get_valores_info_servico(NEW.id_info_servico, NEW_valor_servico, NEW_valor_total);
+
+            -- Obtendo id do servico_realizado
+            SELECT
+                id
+                INTO id_serv_real
+                FROM servico_realizado
+                WHERE
+                    id_info_servico = NEW.id_info_servico
+                LIMIT 1;
+
+            -- Atualizando servico_realizado
+            IF id_serv_real IS NOT NULL THEN
+                UPDATE servico_realizado
+                    SET valor_servico = NEW_valor_servico,
+                        valor_total = NEW_valor_total
+                    WHERE id = id_serv_real;
+            END IF;
+
+            -- Obtendo o id do agendamento
+            SELECT
+                id
+                INTO id_agend
+                FROM agendamento
+                WHERE
+                    id_info_servico = NEW.id_info_servico
+                LIMIT 1;
+
+            IF id_agend IS NOT NULL THEN
+                UPDATE agendamento
+                SET valor_servico = NEW_valor_servico,
+                    valor_total = NEW_valor_total
+                WHERE id = id_agend;
+            END IF;
+        END IF;
+    END;$$
+DELIMITER ;
---
+++ bd/scripts/empresa_schema/criacao/views/create_view_agendamento.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE VIEW vw_agendamento AS
+    SELECT
+        a.id AS id_agendamento,
+        a.dt_hr_marcada AS dt_hr_marcada,
+        a.estado AS estado,
+        i_s.id AS id_info_servico,
+        s_o.id AS id_servico_oferecido,
+        s_o.nome AS nome_servico_oferecido,
+        s_o.id_categoria AS id_categoria_servico_oferecido,
+        c_s.nome AS nome_categoria_servico,
+        COUNT(DISTINCT p_s.id_pet) AS qtd_pet_servico,
+        a.valor_servico AS valor_servico,
+        a.valor_total AS valor_total,
+        i_s.id_funcionario AS id_funcionario,
+        f.nome AS nome_funcionario,
+        i_s.observacoes AS observacoes,
+        a.id_pacote_agend AS id_pacote_agend,
+        a.id_servico_realizado AS id_servico_realizado
+    FROM agendamento AS a
+        INNER JOIN info_servico AS i_s ON (i_s.id = a.id_info_servico)
+            INNER JOIN servico_oferecido AS s_o ON (s_o.id = i_s.id_servico_oferecido)
+                LEFT JOIN categoria_servico AS c_s ON (c_s.id = s_o.id_categoria)
+            INNER JOIN funcionario AS f ON (f.id = i_s.id_funcionario)
+            INNER JOIN pet_servico AS p_s ON (p_s.id_info_servico = i_s.id)
+    GROUP BY a.id
+    ORDER BY
+        id_agendamento DESC;
