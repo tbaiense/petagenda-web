866154072d8c71fd19769544c22d8597ce9dc7db
tbaiense <tbaiense@proton.me>
2025-04-12 00:31:31 -0300

    adiciona view de info_servico e atualiza views de agendamento e servico_realizado para usarem view de info_servico

--- bd/scripts/empresa_schema/criacao/views/create_view_agendamento.sql
+++ bd/scripts/empresa_schema/criacao/views/create_view_agendamento.sql
@@ -3,25 +3,8 @@ CREATE OR REPLACE VIEW vw_agendamento AS
         a.id AS id_agendamento,
         a.dt_hr_marcada AS dt_hr_marcada,
         a.estado AS estado,
-        i_s.id AS id_info_servico,
-        s_o.id AS id_servico_oferecido,
-        s_o.nome AS nome_servico_oferecido,
-        s_o.id_categoria AS id_categoria_servico_oferecido,
-        c_s.nome AS nome_categoria_servico,
-        COUNT(DISTINCT p_s.id_pet) AS qtd_pet_servico,
-        a.valor_servico AS valor_servico,
-        a.valor_total AS valor_total,
-        i_s.id_funcionario AS id_funcionario,
-        f.nome AS nome_funcionario,
-        i_s.observacoes AS observacoes,
-        a.id_pacote_agend AS id_pacote_agend,
-        a.id_servico_realizado AS id_servico_realizado
+        i_s.*
     FROM agendamento AS a
-        INNER JOIN info_servico AS i_s ON (i_s.id = a.id_info_servico)
-            INNER JOIN servico_oferecido AS s_o ON (s_o.id = i_s.id_servico_oferecido)
-                LEFT JOIN categoria_servico AS c_s ON (c_s.id = s_o.id_categoria)
-            LEFT JOIN funcionario AS f ON (f.id = i_s.id_funcionario)
-            INNER JOIN pet_servico AS p_s ON (p_s.id_info_servico = i_s.id)
-    GROUP BY a.id
+        INNER JOIN vw_info_servico AS i_s ON (i_s.id_info_servico = a.id_info_servico)
     ORDER BY
         id_agendamento DESC;
--- bd/scripts/empresa_schema/criacao/views/create_view_servico_realizado.sql
+++ bd/scripts/empresa_schema/criacao/views/create_view_servico_realizado.sql
@@ -3,27 +3,8 @@ CREATE OR REPLACE VIEW vw_servico_realizado AS
         s_r.id AS id_servico_realizado,
         s_r.dt_hr_inicio AS dt_hr_inicio,
         s_r.dt_hr_fim AS dt_hr_fim,
-        i_s.id AS id_info_servico,
-        s_o.id AS id_servico_oferecido,
-        s_o.nome AS nome_servico_oferecido,
-        s_o.id_categoria AS id_categoria_servico_oferecido,
-        c_s.nome AS nome_categoria_servico,
-        COUNT(DISTINCT p_s.id_pet) AS qtd_pet_servico,
-        s_r.valor_servico AS valor_servico,
-        s_r.valor_total AS valor_total,
-        i_s.id_funcionario AS id_funcionario,
-        f.nome AS nome_funcionario,
-        i_s.observacoes AS observacoes
+        i_s.*
     FROM servico_realizado AS s_r
-        INNER JOIN info_servico AS i_s ON (i_s.id = s_r.id_info_servico)
-            INNER JOIN servico_oferecido AS s_o ON (s_o.id = i_s.id_servico_oferecido)
-                LEFT JOIN categoria_servico AS c_s ON (c_s.id = s_o.id_categoria)
-            INNER JOIN funcionario AS f ON (f.id = i_s.id_funcionario)
-            INNER JOIN pet_servico AS p_s ON (p_s.id_info_servico = i_s.id)
-    GROUP BY s_r.id
+        INNER JOIN vw_info_servico AS i_s ON (i_s.id = s_r.id_info_servico)
     ORDER BY 
-        dt_hr_inicio DESC, 
-        dt_hr_fim DESC, 
-        nome_servico_oferecido ASC, 
-        nome_funcionario ASC, 
-        valor_total DESC;
+        dt_hr_fim DESC;
