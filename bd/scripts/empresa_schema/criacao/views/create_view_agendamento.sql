c6082f842f02c4cafa9d19b370f976d07818cf82
tbaiense <thiago.baiense@aluno.senai.br>
2025-04-11 21:43:32 -0300

    atualiza script de criação de schema e cria view de pacote de agendamento, pets do pacote, pets de info_servico

--- bd/scripts/empresa_schema/criacao/views/create_view_agendamento.sql
+++ bd/scripts/empresa_schema/criacao/views/create_view_agendamento.sql
@@ -20,7 +20,7 @@ CREATE OR REPLACE VIEW vw_agendamento AS
         INNER JOIN info_servico AS i_s ON (i_s.id = a.id_info_servico)
             INNER JOIN servico_oferecido AS s_o ON (s_o.id = i_s.id_servico_oferecido)
                 LEFT JOIN categoria_servico AS c_s ON (c_s.id = s_o.id_categoria)
-            INNER JOIN funcionario AS f ON (f.id = i_s.id_funcionario)
+            LEFT JOIN funcionario AS f ON (f.id = i_s.id_funcionario)
             INNER JOIN pet_servico AS p_s ON (p_s.id_info_servico = i_s.id)
     GROUP BY a.id
     ORDER BY
