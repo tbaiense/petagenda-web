a83b04f26c71c570635ffe10d398deb67fe612ec
tbaiense <tbaiense@proton.me>
2025-04-12 08:28:09 -0300

    adiciona verificação de restrições e se pet é do mesmo dono ao trigger de insert da tabela pet_servico

--- bd/scripts/empresa_schema/criacao/criar_schema.sql
+++ bd/scripts/empresa_schema/criacao/criar_schema.sql
@@ -372,12 +372,14 @@ CREATE OR REPLACE VIEW vw_servico_oferecido AS
         s_o.descricao AS descricao,
         s_o.foto AS foto,
         s_o.restricao_participante AS restricao_participante,
-        e.nome AS nome_especie
+        COUNT(r_e.id_servico_oferecido) AS qtd_restr_especie
     FROM
         servico_oferecido AS s_o
         LEFT JOIN categoria_servico AS c_s ON (c_s.id = s_o.id_categoria)
-        CROSS JOIN restricao_especie AS r_e ON (r_e.id_servico_oferecido = s_o.id)
-        LEFT JOIN especie AS e ON (e.id = r_e.id_especie);
+        LEFT JOIN restricao_especie AS r_e ON (r_e.id_servico_oferecido = s_o.id)
+        LEFT JOIN especie AS e ON (e.id = r_e.id_especie)
+    GROUP BY s_o.id
+    ORDER BY nome ASC, preco ASC;
 
 
 CREATE OR REPLACE VIEW vw_restricao_especie_servico AS
@@ -585,14 +587,48 @@ BEGIN
     END IF;
 END;$$
 DELIMITER ;
+
 DELIMITER $$
-CREATE TRIGGER trg_pet_servico_insert /* Faz a atribuição do valor do preço do pet, se aplicável */
+CREATE TRIGGER trg_pet_servico_insert
     BEFORE INSERT
     ON pet_servico
     FOR EACH ROW
     BEGIN
+        -- Variáveis de definição do valor_pet
         DECLARE tipo_p VARCHAR(16); /*Tipo da cobrança do serviço*/
         DECLARE p DECIMAL(8,2); /* Preço cobrado pelo serviço */
+
+        -- Variáveis de validação do dono
+        DECLARE id_cli_este INT; /* cliente associado a este pet */
+        DECLARE id_pet_outro INT; /* id de outro pet associado a este info_servico */
+        DECLARE id_cli_outro INT; /* cliente associado a outro pet do info_servico*/
+
+        -- Variáveis de validação da espécie
+        DECLARE id_ser_ofer INT;
+        DECLARE id_esp_este INT;
+        DECLARE id_esp_cur INT;
+        DECLARE cur_done INT DEFAULT FALSE;
+        -- Variáveis de validação de participantes
+        DECLARE restr_partic ENUM("individual", "coletivo");
+
+        -- Condições de erro
+        DECLARE err_dono_diferente CONDITION FOR SQLSTATE '45000'; /* pet inserido pertence a outro dono */
+        DECLARE err_esp_incompativel CONDITION FOR SQLSTATE '45001'; /* espécie do pet é incompatível com as das restrições de espécie aplicadas */
+        DECLARE err_qtd_partic_excedido CONDITION FOR SQLSTATE '45002'; /* não é possível adicionar outro pet, devido à restriçao de participantes aplicada  */
+
+         -- Cursores
+        DECLARE cur_especie CURSOR FOR
+            SELECT
+                id_especie
+                FROM restricao_especie
+                WHERE id_servico_oferecido = (
+                    SELECT id_servico_oferecido
+                        FROM info_servico
+                        WHERE id = NEW.id_info_servico
+                );
+
+        -- Handlers
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET cur_done = TRUE;
 
         -- Obtendo informação sobre a forma de cobrança do serviço
         SELECT
@@ -613,6 +649,53 @@ CREATE TRIGGER trg_pet_servico_insert /* Faz a atribuição do valor do preço d
         ELSE
             SET NEW.valor_pet = NULL;
         END IF;
+
+        -- Buscando o id de outro pet existe para o mesmo info_servico
+        SELECT id_pet
+            INTO id_pet_outro
+            FROM pet_servico
+            WHERE
+                id_info_servico = NEW.id_info_servico
+            LIMIT 1;
+
+        -- Validação dos participantes do info_servico
+        SELECT restricao_participante INTO restr_partic FROM servico_oferecido WHERE id = (
+            SELECT id_servico_oferecido FROM info_servico WHERE id = NEW.id_info_servico;
+        );
+
+        IF id_pet_outro IS NOT NULL THEN /* Já existe outro pet para o info_servico */
+
+            -- Validação se o pet pertence ao mesmo dono
+            SELECT id_cliente, id_especie INTO id_cli_este, id_esp_este FROM pet WHERE id = NEW.id_pet;
+            SELECT id_cliente INTO id_cli_outro FROM pet WHERE id = id_pet_outro
+
+            IF id_cli_este <> id_cli_outro THEN
+                SIGNAL err_dono_diferente
+                    SET MESSAGE_TEXT = "Pet nao pode ser inserido, pois pertence a um dono diferente dos que já existem para este info_servico";
+            END IF;
+
+            IF restr_partic = "individual" THEN
+                SIGNAL err_qtd_partic_excedido
+                    SET MESSAGE_TEXT = "Nao e permitido adicionar pet, pois o servico_oferecido possui restricao individual";
+            END IF;
+        END IF;
+
+
+        -- Validação da espécie do pet
+        OPEN cur_especie;
+        especie_loop: LOOP
+            FETCH cur_especie INTO id_esp_cur;
+
+            IF cur_done THEN
+                LEAVE especie_loop;
+            END IF;
+
+            IF id_esp_cur <> id_esp_este THEN
+                SIGNAL err_esp_incompativel
+                    SET MESSAGE_TEXT = "Especie do pet inserido e incompativel com restricoes de especie do servico_oferecido";
+            END IF;
+        END LOOP;
+        CLOSE cur_especie;
     END;$$
 DELIMITER ;
 
