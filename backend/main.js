eeb48bb96f3a7081fa21d12541f54625c38d30e5
tbaiense <tbaiense@proton.me>
2025-05-09 15:18:32 -0300

    atualiza backend

--- backend/main.js
+++ backend/main.js
@@ -1,84 +1,26 @@
-import http from "node:http";
-import { Buffer } from "node:buffer";
-import mysqlx from "@mysql/xdevapi";
+/*
+Script de entry-point para o servidor de back-end para o PetAgenda.
+=========================================================================
+Versão: 0.0.1
+Data: 28/04/2025
+Descrição:
+    Define as configurações do servidor HTTP.
+=========================================================================
+*/
+import http from 'node:http';
+import router from './router.js';
 
-const dbConfig = {
-    password: process.env.DB_PASS,
-    user: "root",
-    host: "localhost",
-    port: 33060,
-    schema: "dbo",
-};
+const PORT = 3000;
+const ADDRESS = 'localhost';
 
 const app = http.createServer((req, res) => {
-    const path = req.url;
-    const method = req.method;
-    const contentType = req.headers['content-type'];
-
-    if (path == '/cadastro') {
-        if (method == "POST") {
-            const bufferArray = [];
-            let body;
-
-            req.on('data', chunk => {
-                bufferArray.push(chunk);
-            })
-
-            if (contentType == "application/x-www-form-urlencoded") {
-                req.on('end', () => {
-                    body = Buffer.concat(bufferArray).toString();
-
-                    const params = new URLSearchParams(body);
-
-                    console.log(params);
-                    console.log(dbConfig);
-
-                    const usr = {
-                        email: params.get("email"),
-                        senha: params.get("senha"),
-                        perguntaSeguranca: {
-                            pergunta: params.get("perg-seg"),
-                            resposta: params.get("resp-seg")
-                        }
-                    }
-
-                    const usrJson = JSON.stringify(usr);
-
-                    mysqlx.getSession(dbConfig)
-                        .then(session => {
-                            return session.startTransaction()
-                                .then(() => {
-                                    return session.sql(`CALL usuario('insert', '${usrJson}')`)
-                                        .execute();
-                                })
-                                .then(result => {
-                                    console.log(result.fetchOne());
-                                    return session.commit();
-                                })
-                                .catch(err => {
-                                    console.log(err.info.msg);
-                                    return session.rollback();
-                                })
-                                .then(() => {
-                                    res.write("sucess");
-                                    res.statusCode = 200;
-                                    res.setHeader("Content-Type", "text/plain");
-
-                                    res.end();
-                                });
-                        });
-                });
-            } else {
-                res.statusCode = 400;
-                res.end();
-            }
-
-        }
-    } else {
-        res.statusCode = 404;
+    try {
+        router(req, res);
+    } catch (err) {
+        console.error(err.message);
+        res.statusCode = 500;
         res.end();
     }
-
 });
 
-app.listen(8080, '127.0.0.1');
+app.listen(PORT, ADDRESS);
