7fa563255abf12db190ba9fc7c35241aee050bca
castro <castro.nunes@aluno.senai.br>
2025-05-29 15:52:18 -0300

    Concertando erro

--- PetAgenda-Projeto/src/components/Endereco/CamposEndereco.jsx
+++ PetAgenda-Projeto/src/components/Endereco/CamposEndereco.jsx
@@ -1,37 +1,47 @@
 import { UFS } from "../../data/info"
-
+import styles from "./CamposEndereco.module.css"
 
 const CamposEndereco = ({index, register}) => {
     console.log("Renderizando endereço", index);
     return(
-        <>
-            <div>
-                <label htmlFor="">Logradouro</label>
-                <input type="text" />
-            </div>
+<div className={styles.enderecoContainer}>
 
-            <div>
-                <label htmlFor="">Número</label>
-                <input type="text" />
-            </div>
+  <div className={styles.linhaEndereco}>
+    <div className={`${styles.labelInput} ${styles.flex5}`}>
+      <label>Logradouro</label>
+      <input type="text" placeholder="Digite o logradouro" />
+    </div>
 
-            <div>
-                <label htmlFor="">Bairro</label>
-                <input type="text" />
-            </div>
+    <div className={`${styles.labelInput} ${styles.flex1}`}>
+      <label>Número</label>
+      <input type="text" placeholder="Número" />
+    </div>
 
-            <div>
-                <label htmlFor="">Cidade</label>
-                <input type="text" />
-            </div>
+    <div className={`${styles.labelInput} ${styles.flex1}`}>
+      <label>UF</label>
+      <select>
+        <option value="">UF</option>
+        {UFS.map((uf, index) => (
+          <option key={index} value={uf}>{uf}</option>
+        ))}
+      </select>
+    </div>
+  </div>
 
-            <select >
-                <option value="">UF</option>
-                {UFS.map((UF, index) => (
-                    <option key={index} value={UF}>{UF}</option>
-                ))}
-            </select>
-        </>
+  <div className={styles.linhaEndereco}>
+    <div className={`${styles.labelInput} ${styles.flex2}`}>
+      <label>Bairro</label>
+      <input type="text" placeholder="Digite o bairro" />
+    </div>
+
+    <div className={`${styles.labelInput} ${styles.flex3}`}>
+      <label>Cidade</label>
+      <input type="text" placeholder="Digite a cidade" />
+    </div>
+  </div>
+
+</div>
+
     )
 }
 
