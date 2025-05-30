c5daa206c50e316d45dab6101e513d18fed90b0e
tbaiense <thiago.baiense@aluno.senai.br>
2025-05-30 11:43:31 -0300

    atualiza trigger de inserção de pet em pacote de agendamento

--- bd/scripts/empresa_schema/criacao/triggers/create_trigger_pet_pacote.sql
+++ bd/scripts/empresa_schema/criacao/triggers/create_trigger_pet_pacote.sql
@@ -12,8 +12,13 @@ CREATE TRIGGER trg_pet_pacote_insert /* Validação semelhante à aplicada à ta
         -- Variáveis de validação da espécie
         DECLARE id_ser_ofer INT;
         DECLARE id_esp_este INT;
+        DECLARE id_esp_outro INT;
         DECLARE id_esp_cur INT;
         DECLARE cur_done INT DEFAULT FALSE;
+        DECLARE serv_tem_restr_esp INT DEFAULT FALSE;
+        DECLARE validar_esp INT DEFAULT TRUE;
+        DECLARE esp_valida INT DEFAULT FALSE;
+        
         -- Variáveis de validação de participantes
         DECLARE restr_partic ENUM("individual", "coletivo");
 
@@ -50,38 +55,59 @@ CREATE TRIGGER trg_pet_pacote_insert /* Validação semelhante à aplicada à ta
             SELECT id_servico_oferecido FROM pacote_agend WHERE id = NEW.id_pacote_agend
         );
 
+	    -- Obtém as informações do PET sendo inserido
+        SELECT id_cliente, id_especie INTO id_cli_este, id_esp_este FROM pet WHERE id = NEW.id_pet;
+	    
         IF id_pet_outro IS NOT NULL THEN /* Já existe outro pet para o pacote_agend */
 
+            IF restr_partic = "individual" THEN
+                SIGNAL err_qtd_partic_excedido
+                    SET MESSAGE_TEXT = "Nao e permitido adicionar pet, pois o servico_oferecido possui restricao individual";
+            END IF;
+            
             -- Validação se o pet pertence ao mesmo dono
-            SELECT id_cliente, id_especie INTO id_cli_este, id_esp_este FROM pet WHERE id = NEW.id_pet;
             SELECT id_cliente INTO id_cli_outro FROM pet WHERE id = id_pet_outro;
 
             IF id_cli_este <> id_cli_outro THEN
                 SIGNAL err_dono_diferente
                     SET MESSAGE_TEXT = "Pet nao pode ser inserido, pois pertence a um dono diferente dos que já existem para este pacote_agend";
             END IF;
-
-            IF restr_partic = "individual" THEN
-                SIGNAL err_qtd_partic_excedido
-                    SET MESSAGE_TEXT = "Nao e permitido adicionar pet, pois o servico_oferecido possui restricao individual";
+            
+            SELECT id_especie INTO id_esp_outro FROM pet WHERE id = id_pet_outro LIMIT 1;
+            
+            IF id_esp_este = id_esp_outro THEN
+            	SET validar_esp = FALSE;
             END IF;
+
         END IF;
 
 
         -- Validação da espécie do pet
-        OPEN cur_especie;
-        especie_loop: LOOP
-            FETCH cur_especie INTO id_esp_cur;
-
-            IF cur_done THEN
-                LEAVE especie_loop;
-            END IF;
-
-            IF id_esp_cur <> id_esp_este THEN
-                SIGNAL err_esp_incompativel
-                    SET MESSAGE_TEXT = "Especie do pet inserido e incompativel com restricoes de especie do servico_oferecido";
-            END IF;
-        END LOOP;
-        CLOSE cur_especie;
+        IF validar_esp IS TRUE THEN
+	        OPEN cur_especie;
+	        especie_loop: LOOP
+	            FETCH cur_especie INTO id_esp_cur;
+        		
+        		IF id_esp_cur IS NOT NULL THEN
+		            SET serv_tem_restr_esp = TRUE;
+        		
+		            IF id_esp_cur = id_esp_este THEN
+		            	SET esp_valida = TRUE;
+		            	LEAVE especie_loop;
+		            END IF;
+        		END IF;
+        		
+	            IF cur_done THEN
+	                LEAVE especie_loop;
+	            END IF;
+				
+	        END LOOP;
+	        CLOSE cur_especie;
+	        
+	        IF serv_tem_restr_esp IS TRUE AND esp_valida IS FALSE THEN
+		        SIGNAL err_esp_incompativel
+		            SET MESSAGE_TEXT = "Especie do pet inserido e incompativel com restricoes de especie do servico_oferecido";
+	        END IF;
+        END IF;
     END;$$
 DELIMITER ;
