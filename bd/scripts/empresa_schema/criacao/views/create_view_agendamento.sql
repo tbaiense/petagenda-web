c3d6759cbd30a5423b964bc173ed0022d316b26c
tbaiense <thiago.baiense@aluno.senai.br>
2025-05-30 20:39:57 -0300

    atualiza view de agendamento

--- bd/scripts/empresa_schema/criacao/views/create_view_agendamento.sql
+++ bd/scripts/empresa_schema/criacao/views/create_view_agendamento.sql
@@ -6,6 +6,7 @@ CREATE OR REPLACE VIEW vw_agendamento AS
         a.id_pacote_agend AS id_pacote_agend,
         a.valor_servico AS valor_servico,
 		a.valor_total AS valor_total,
+		a.id_servico_realizado AS id_servico_realizado,
         i_s.*
     FROM agendamento AS a
         INNER JOIN vw_info_servico AS i_s ON (i_s.id_info_servico = a.id_info_servico)
