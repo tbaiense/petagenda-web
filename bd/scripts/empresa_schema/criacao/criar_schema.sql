898705925d8915196e9a8409950a403d59732ed0
tbaiense <thiago.baiense@aluno.senai.br>
2025-05-30 11:28:26 -0300

    corrige verificação de restriçao de espécie em agendametos e serviços executados

--- bd/scripts/empresa_schema/criacao/criar_schema.sql
+++ bd/scripts/empresa_schema/criacao/criar_schema.sql
@@ -702,8 +702,13 @@ CREATE TRIGGER trg_pet_servico_insert
         -- Variáveis de validação da espécie
         DECLARE id_ser_ofer INT;
         DECLARE id_esp_este INT;
+        DECLARE id_esp_outro INT;
         DECLARE id_esp_cur INT;
         DECLARE cur_done INT DEFAULT FALSE;
+        DECLARE serv_tem_restr_esp INT DEFAULT FALSE;
+        DECLARE validar_esp INT DEFAULT TRUE;
+        DECLARE esp_valida INT DEFAULT FALSE;
+        
         -- Variáveis de validação de participantes
         DECLARE restr_partic ENUM("individual", "coletivo");
 
@@ -739,17 +744,13 @@ CREATE TRIGGER trg_pet_servico_insert
                 FROM info_servico
                 WHERE id = NEW.id_info_servico 
         LIMIT 1);
-
-        SET @rodei = FALSE;
-    	SET @serv = NEW.id_info_servico;
         
         IF tipo_p = 'pet' THEN
-        	SET @rodei = TRUE;
             SET NEW.valor_pet = p;
         ELSE
             SET NEW.valor_pet = NULL;
         END IF;
-		SET @valor_pet = p;
+        
         -- Buscando o id de outro pet existe para o mesmo info_servico
         SELECT id_pet
             INTO id_pet_outro
@@ -763,39 +764,58 @@ CREATE TRIGGER trg_pet_servico_insert
             SELECT id_servico_oferecido FROM info_servico WHERE id = NEW.id_info_servico
         );
 
+        -- Obtém as informações do PET sendo inserido
+        SELECT id_cliente, id_especie INTO id_cli_este, id_esp_este FROM pet WHERE id = NEW.id_pet;
+        
         IF id_pet_outro IS NOT NULL THEN /* Já existe outro pet para o info_servico */
-
-            -- Validação se o pet pertence ao mesmo dono
-            SELECT id_cliente, id_especie INTO id_cli_este, id_esp_este FROM pet WHERE id = NEW.id_pet;
-            SELECT id_cliente INTO id_cli_outro FROM pet WHERE id = id_pet_outro;
-
-            IF id_cli_este <> id_cli_outro THEN
-                SIGNAL err_dono_diferente
-                    SET MESSAGE_TEXT = "Pet nao pode ser inserido, pois pertence a um dono diferente dos que já existem para este info_servico";
-            END IF;
 
             IF restr_partic = "individual" THEN
                 SIGNAL err_qtd_partic_excedido
                     SET MESSAGE_TEXT = "Nao e permitido adicionar pet, pois o servico_oferecido possui restricao individual";
             END IF;
+            
+            -- Validação se o pet pertence ao mesmo dono
+            SELECT id_cliente INTO id_cli_outro FROM pet WHERE id = id_pet_outro;
+            
+            IF id_cli_este <> id_cli_outro THEN
+                SIGNAL err_dono_diferente
+                    SET MESSAGE_TEXT = "Pet nao pode ser inserido, pois pertence a um dono diferente dos que já existem para este info_servico";
+            END IF;
+
+        	SELECT id_especie INTO id_esp_outro FROM pet WHERE id = id_pet_outro LIMIT 1;
+            
+            IF id_esp_este = id_esp_outro THEN
+            	SET validar_esp = FALSE;
+            END IF;
         END IF;
 
-
         -- Validação da espécie do pet
-        OPEN cur_especie;
-        especie_loop: LOOP
-            FETCH cur_especie INTO id_esp_cur;
-
-            IF cur_done THEN
-                LEAVE especie_loop;
-            END IF;
-
-            IF id_esp_cur <> id_esp_este THEN
-                SIGNAL err_esp_incompativel
-                    SET MESSAGE_TEXT = "Especie do pet inserido e incompativel com restricoes de especie do servico_oferecido";
-            END IF;
-        END LOOP;
-        CLOSE cur_especie;
+        IF validar_esp IS TRUE THEN
+	        OPEN cur_especie;
+	        especie_loop: LOOP
+	            FETCH cur_especie INTO id_esp_cur;
+        		
+        		IF id_esp_cur IS NOT NULL THEN
+		            SET serv_tem_restr_esp = TRUE;
+        		
+		            IF id_esp_cur = id_esp_este THEN
+		            	SET esp_valida = TRUE;
+		            	LEAVE especie_loop;
+		            END IF;
+        		END IF;
+        		
+	            IF cur_done THEN
+	                LEAVE especie_loop;
+	            END IF;
+				
+	        END LOOP;
+	        CLOSE cur_especie;
+	        
+	        IF serv_tem_restr_esp IS TRUE AND esp_valida IS FALSE THEN
+		        SIGNAL err_esp_incompativel
+		            SET MESSAGE_TEXT = "Especie do pet inserido e incompativel com restricoes de especie do servico_oferecido";
+	        END IF;
+        END IF;
     END;$$
 DELIMITER ;
 
