e89367d10b4bbd56ab7b9d9c83243a39ac489571
tbaiense <tbaiense@proton.me>
2025-06-06 02:12:36 -0300

    comenta private router

--- PetAgenda-Projeto/src/routers/OurRouters.jsx
+++ PetAgenda-Projeto/src/routers/OurRouters.jsx
@@ -60,9 +60,9 @@ const router = createBrowserRouter([
   {
     path: "/empresa",
     element: (
-       <PrivateRoute>
+       //<PrivateRoute>
         <MenuDashBoard />
-        </PrivateRoute>
+        //</PrivateRoute>
     ),
     children: [
       // Empresa
