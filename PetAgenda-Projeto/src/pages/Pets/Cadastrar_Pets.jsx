9c04d93322e72f40475e8db47b085f9e8a3b6e1d
tbaiense <tbaiense@proton.me>
2025-06-09 01:18:16 -0300

    adiciona cadastro de pet

--- PetAgenda-Projeto/src/pages/Pets/Cadastrar_Pets.jsx
+++ PetAgenda-Projeto/src/pages/Pets/Cadastrar_Pets.jsx
@@ -5,12 +5,11 @@ import { useAuth } from '../../contexts/UserContext';
 import { useForm } from "react-hook-form";
 
 const CadastrarPets = () => {
-  const navigate = useNavigate();
 
-  // const locate = useLocation()
-  // const infoCliente = locate.state
+  const navigate = useNavigate();
   const [clientes, setClientes] = useState([])
   const [ clienteEscolhido, setClienteEscolhido ] = useState({});
+  const [ especies, setEspecies ] = useState();
   const [ pet, setPet ] = useState();
   const { empresaFetch } = useAuth();
   const [aceito, setAceito] = useState(false);
@@ -23,23 +22,9 @@ const CadastrarPets = () => {
       watch
   } = useForm();
 
-
-  // Verifica se há dados da pagina de cliente
-  // if (!infoCliente) {
-  //   // Se chegou até aqui e porque o usuario não cadastrou um cliente 
-  //   // Logo, a API vai pegar todos os clientes para carregar a comboBox
-  //   empresaFetch('/cliente')
-  //   // Converto os clientes em JSON 
-  //   .then(res => res.json())
-  //   .then(data => {
-  //       setServicos(data);
-  //   })
-  //   .catch(error => {
-  //       console.error("Erro ao buscar Clientes:", error);
-  //   });
-  // }
-
   const cadastrarPet = async (pet) => {
+    const jsonPet = JSON.stringify(pet);
+    console.log("novo: ",jsonPet);
     try {
       const res = await empresaFetch('/pet', {
           method: "POST",
@@ -49,7 +34,7 @@ const CadastrarPets = () => {
       const jsonBody = await res.json();
 
       if (res.status == 200) {
-        const idPet = jsonBody.id;
+        const idPet = jsonBody.pet.id;
 
         alert('cadastrado');
         navigate(`/empresa/pets/${idPet}`);
@@ -62,14 +47,109 @@ const CadastrarPets = () => {
   };
 
   const onSubmit = (data) => {
-    setPet(data); // TODO: criar objeto certo para cadastro de pet
+    const newPet = {
+      dono: {
+          id: data.clienteId
+      },
+      especie: {
+          id: data.especie
+      },
+      nome: data.nome,
+      sexo: data.sexo,
+      porte: data.porte,
+      eCastrado: data.eCastrado,
+      estadoSaude: "saudável",
+      raca: "",
+      cor: "",
+      comportamento: "",
+      cartaoVacina: null
+    };
+    console.log("novo: ", newPet);
 
-    cadastrarPet(pet);
+    cadastrarPet(newPet);
   }
 
   const onErrors = (errors) => {
     console.log(errors)
   }
+
+  async function obterClientes() {
+    try {
+      const cliResp = await empresaFetch('/cliente');
+
+      if (cliResp.status == 200) {
+        const jsonBody = await cliResp.json();
+
+        if (!jsonBody) {
+          throw new Error("servidor enviou corpo vazio");
+        }
+
+        if (!jsonBody.clientes) {
+          throw new Error(jsonBody.message);
+        }
+
+        if (jsonBody.clientes.length == 0) {
+          return [];
+        } else {
+          return jsonBody.clientes;
+        }
+      } else {
+        throw new Error('requisição não retornou código 200');
+      }
+    } catch (err) {
+      err.message = "Falha ao obter clientes cadastrados: " + err.message;
+      throw err;
+      // return [];
+    }
+  }
+
+  async function obterEspecies() {
+    try {
+      const espResp = await empresaFetch('/pet/especie');
+
+      if (espResp.status == 200) {
+        const jsonBody = await espResp.json();
+        if (!jsonBody) {
+          throw new Error("servidor enviou corpo vazio");
+        }
+        
+        if (!jsonBody.especiesPet) {
+          throw new Error(jsonBody.message);
+        }
+
+        return jsonBody.especiesPet;
+
+      } else {
+        throw new Error('requisição não retornou código 200');
+      }
+    } catch (err) {
+      err.message = "Falha ao obter espécies cadastradas: " + err.message;
+      throw err;
+      // return [];
+    }
+  }
+
+  
+  useEffect(() => {
+    // Populando clientes
+    obterClientes()
+      .then( cliList => {
+        setClientes(cliList);
+        console.log('clientes encontrados: ', cliList);
+      })
+      .catch( err => {
+        alert(err.message);
+      });
+
+    // Populando espécies
+    obterEspecies()
+      .then( espList => {
+        setEspecies(espList);
+      })
+      .catch( err => {
+        alert(err.message);
+      });
+  }, []);
 
   return (
     <div> 
@@ -88,37 +168,49 @@ const CadastrarPets = () => {
             <div className={styles.estiloCampos}>
               <label>Dono</label>
               <select {...register("clienteId", { required: true })}>
-                {/* {infoCliente ? (
-                  <option value={infoCliente.id}>{infoCliente.nome}</option>
-                ) : (
-                  <>
-                    <option value="">Selecione um dono</option>
-                    {clientes.map((cliente, index) => (
-                      <option value={cliente.id} key={index}>{cliente.nome}</option>
+                <option value="">Selecione um dono</option>
+                    {clientes.map((cliente) => (
+                      <option value={cliente.id} key={cliente.id}>{cliente.nome}</option>
                     ))}
-                  </>
-                )} */}
               </select>
             </div>
 
             <div className={styles.estiloCampos}>
               <label>Nome do Pet</label>
-              <input type="text" placeholder="Digite o nome do pet" {...register("nomePet", { required: true })} />
+              <input type="text" placeholder="Digite o nome do pet" {...register("nome", { 
+                required: { value: true, message: "Nome do pet é obrigatório"},
+                maxLength: { value: 64, message: "Nome deve ser de até 64 caracteres"}
+                })} />
             </div>
           </div>
 
 
           <div className={styles.linhaDoisCampos}>
-
-            {/* Colocar um select em vez de input */}
             <div className={styles.estiloCampos}>
               <label>Espécie</label>
-              <input type="text" placeholder="Digite a espécie" {...register("especie", { required: true })} />
+              <select id="especie-select" {...register("especie", {
+                required: {
+                  value: true,
+                  message: "Espécie do Pet precisa ser definida"
+                }
+              })}>
+                { especies && especies.map( esp => {
+                  return (
+                    <option value={esp.id} key={esp.id}>{esp.nome}</option>
+                  );
+                })}
+              </select>
             </div>
 
             <div className={styles.estiloCampos}>
               <label>Raça</label>
-              <input type="text" placeholder="Digite a raça" {...register("raca", { required: true })} />
+              <input type="text" placeholder="Digite a raça" {...register("raca", { 
+                required: false,
+                maxLength: {
+                  value: 64,
+                  message: "Tamanho máximo é de 64 caracteres."
+                } 
+                })} />
             </div>
 
           </div>
@@ -126,15 +218,15 @@ const CadastrarPets = () => {
           <div className={styles.linhaDoisCampos}>
             <div className={styles.estiloCampos}>
                 <label htmlFor="">Cor</label>
-                <input type="text" name="" id="" placeholder="Digite a cor" {...register("raca", { required: true })}/>
+                <input type="text" name="" id="" placeholder="Escreva a cor" {...register("cor", { required: false, maxLength: { value: 16, message: "Cor deve ser entre 0 e 16 caracteres."} })}/>
             </div>
             <div className={styles.estiloCampos}>
                 <label htmlFor="">Porte</label>
-                <select name="" id="" {...register("raca", { required: true })}>
+                <select name="porte" id="" {...register("porte", { required: true })}>
                   <option value="">Selecione</option>
-                  <option value="">Grande Porte</option>
-                  <option value="">Médio Porte</option>
-                  <option value="">Pequeno Porte</option>
+                  <option value="G">Grande</option>
+                  <option value="M">Médio</option>
+                  <option value="P">Pequeno</option>
                 </select>
             </div>
           </div>
@@ -143,16 +235,18 @@ const CadastrarPets = () => {
           <div className={styles.linhaComCheckbox}>
 
             <div className={styles.estiloCampos}>
-              <input type="checkbox" checked={aceito} onChange={(e) => setAceito(e.target.checked)} />
+              <input type="checkbox" checked={aceito} onChange={(e) => setAceito(e.target.checked)} 
+                { ...register("eCastrado", { required: false }) }
+              />
               <label style={{ marginLeft: "0.5rem" }}>Castrado?</label>
             </div>
 
             <div className={styles.estiloCampos}>
               <label>Sexo</label>
-              <select {...register("sexo", { required: true })}>
+              <select {...register("sexo", { required: {value: true, message: "O sexo do Pet precisa ser definido"} })}>
                 <option value="">Selecione</option>
-                <option value="M">Masculino</option>
-                <option value="F">Feminino</option>
+                <option value="M">Macho</option>
+                <option value="F">Fêmea</option>
               </select>
             </div>
 
@@ -168,8 +262,8 @@ const CadastrarPets = () => {
             <textarea className={styles.inputTextArea} placeholder="Descreva o comportamento do pet"
               {...register("comportamento", {
                 maxLength: {
-                  value: 150,
-                  message: "O máximo de caracteres é 150."
+                  value: 64,
+                  message: "O máximo de caracteres é 64."
                 }
               })}
             />
@@ -190,11 +284,11 @@ const CadastrarPets = () => {
             </div>
 
             <label>Saúde</label>
-            <textarea className={styles.inputTextArea} placeholder="Descreva a saúde do pet"
-              {...register("saude", {
+            <textarea className={styles.inputTextArea} placeholder="Descreva o estado de saúde do pet"
+              {...register("estadoSaude", {
                 maxLength: {
-                  value: 150,
-                  message: "O máximo de caracteres é 150."
+                  value: 32,
+                  message: "O máximo de caracteres é 32."
                 }
               })}
             />
