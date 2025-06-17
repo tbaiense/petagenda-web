5e2341eca7802175384ef234625d364c46a61c0a
tbaiense <thiago.baiense@aluno.senai.br>
2025-06-17 20:13:25 -0300

    implementa listagem de pets

--- PetAgenda-Projeto/src/components/CardPet/PetListaCard.jsx
+++ PetAgenda-Projeto/src/components/CardPet/PetListaCard.jsx
@@ -4,25 +4,25 @@ import male from '../../assets/icon_male.svg';
 import female from '../../assets/icon_female.svg';
 import styles from './PetListaCard.module.css';
 
-const PetListaCard = ({ nome, especie, dono, sexo}) => {
+const PetListaCard = ({ pet: p, showInfo}) => {
   return (
     <div className={styles.miniCard}>
         <div className={styles.suporte}>
             <div className={styles.male}>
-                <img src={(sexo == 'M') ? male : female} alt=""/>
+                <img src={(p.sexo == 'M') ? male : female} alt=""/>
             </div>
 
             <div className={styles.subInfo}>
-                <span className={styles.estiloNome}>{nome}</span>
-                <span className={styles.estiloEspecie}>{especie}</span>
-                <span className={styles.estiloDono}>{dono}</span>
+                <span className={styles.estiloNome}>{p.nome}</span>
+                <span className={styles.estiloEspecie}>{p.especie}</span>
+                <span className={styles.estiloDono}>{p.dono}</span>
             </div>
         </div>
         <div>
-            <button onClick={() => setShowInfo(true)} className={styles.bttn}><img src={seta} alt="seta" /></button>
+            <button onClick={() => showInfo()} className={styles.bttn}><img src={seta} alt="seta" /></button>
         </div>
     </div>
   )
 }
 
-export default PetListaCard
+export default PetListaCard;
