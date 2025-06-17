e9da1d4cfd9bd1b609d69c0a0fa704b3e839b7e4
Matheus <matheus.schmidt@aluno.senai.br>
2025-06-17 20:47:31 -0300

    alterando rotas card agendamento

--- PetAgenda-Projeto/src/components/CardAgendamento/CardAgendamento.jsx
+++ PetAgenda-Projeto/src/components/CardAgendamento/CardAgendamento.jsx
@@ -1,60 +1,73 @@
-
-import React, { useEffect, useState } from 'react'
-import { Button } from 'react-bootstrap';
-import Modal_Atualizar_Rapido_Agendamento from '../../pages/Agendamentos/Lista/Modal_Atualizar_Rapido_Agendamento';
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import Modal_Atualizar_Rapido_Agendamento from "../../pages/Agendamentos/Lista/Modal_Atualizar_Rapido_Agendamento";
 
 const CardAgendamento = ({ agendamento, funcDisponiveis }) => {
-    const [ estado, setEstado ] = useState({});
-    const [ funcionario, setFuncionario ] = useState({});
+  const [estado, setEstado] = useState({});
+  const [funcionario, setFuncionario] = useState({});
 
-    const [show, setShow] = useState(false);
-    
-    const handleShowEditarModal = () => {
-        setShow(true);
-    }
+  const [show, setShow] = useState(false);
 
-    // useEffect(() => {
-    //     console.log('rodei func');
-    //     if (agendamento.funcionario?.nome) 
-    //     setEstado(agendamento.funcionario);
-    // }, [agendamento.funcionario.id]);
+  const handleShowEditarModal = () => {
+    setShow(true);
+  };
 
-    // useEffect(() => {
-    //     console.log('rodei est');
-    //     setEstado(agendamento.estado);
-    // }, [agendamento.estado.id]);
+  // useEffect(() => {
+  //     console.log('rodei func');
+  //     if (agendamento.funcionario?.nome)
+  //     setEstado(agendamento.funcionario);
+  // }, [agendamento.funcionario.id]);
 
-    useEffect(() => {
-        setEstado(agendamento.estado);
-        if (agendamento.funcionario?.nome) setFuncionario(agendamento.funcionario);
-    });
+  // useEffect(() => {
+  //     console.log('rodei est');
+  //     setEstado(agendamento.estado);
+  // }, [agendamento.estado.id]);
+
+  useEffect(() => {
+    setEstado(agendamento.estado);
+    if (agendamento.funcionario?.nome) setFuncionario(agendamento.funcionario);
+  });
   return (
     <>
-        <tr>
-            <td>{agendamento.servico.nome}</td>
-            <td>{agendamento.cliente.nome}</td>
-            <td>{funcionario.nome}</td>
-            <td><div>{agendamento.dtHrMarcada.replace(/ \d\d:\d\d:\d\d/, '')}</div><br></br>~2 meses</td>
-            <td>{agendamento.dtHrMarcada.replace(/\d{4}-\d{2}-\d{2} /, '')}</td>
-            <td>{estado.id}</td>
-            <td>{`R$ ${agendamento.valor.total}`}</td>
-            <td>
-                <Button className="form-button" variant="primary" onClick={handleShowEditarModal}>Editar</Button>
-                <Button className="form-button" variant="success">Detalhes</Button>
-            </td>
-        </tr>
-        <Modal_Atualizar_Rapido_Agendamento
-            funcDisponiveis={funcDisponiveis} 
-            agendamento={agendamento} 
-            estado={estado} 
-            setEstado={setEstado} 
-            funcionario={funcionario} 
-            setFuncionario={setFuncionario} 
-            show={show} 
-            setShow={setShow}>
-        </Modal_Atualizar_Rapido_Agendamento>
+      <tr>
+        <td>{agendamento.servico.nome}</td>
+        <td>{agendamento.cliente.nome}</td>
+        <td>{funcionario.nome}</td>
+        <td>
+          <div>{agendamento.dtHrMarcada.replace(/ \d\d:\d\d:\d\d/, "")}</div>
+          <br></br>~2 meses
+        </td>
+        <td>{agendamento.dtHrMarcada.replace(/\d{4}-\d{2}-\d{2} /, "")}</td>
+        <td>{estado.id}</td>
+        <td>{`R$ ${agendamento.valor.total}`}</td>
+        <td>
+          <Button
+            className="form-button"
+            variant="primary"
+            onClick={() => {
+              handleShowEditarModal();
+              navigate("/empresa/agendamentos/editar/:" + agendamento.id);
+            }}
+          >
+            Editar
+          </Button>
+          <Button className="form-button" variant="success">
+            Detalhes
+          </Button>
+        </td>
+      </tr>
+      <Modal_Atualizar_Rapido_Agendamento
+        funcDisponiveis={funcDisponiveis}
+        agendamento={agendamento}
+        estado={estado}
+        setEstado={setEstado}
+        funcionario={funcionario}
+        setFuncionario={setFuncionario}
+        show={show}
+        setShow={setShow}
+      ></Modal_Atualizar_Rapido_Agendamento>
     </>
-    );
-}
+  );
+};
 
-export default CardAgendamento
+export default CardAgendamento;
