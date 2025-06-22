669a7280aa2d5670d971f766c66dcd6c995a0817
Matheus <matheus.schmidt@aluno.senai.br>
2025-06-22 03:07:30 -0300

    desing pags listar pets

--- PetAgenda-Projeto/src/components/CardPet/PetListaCard.jsx
+++ PetAgenda-Projeto/src/components/CardPet/PetListaCard.jsx
@@ -2,28 +2,59 @@ import React from "react";
 import seta from "../../assets/icon_seta.svg";
 import male from "../../assets/icon_male.svg";
 import female from "../../assets/icon_female.svg";
+import iconEditar from "../../assets/icon_editarAzul.svg";
+import iconDeletar from "../../assets/icon_delete.svg";
 import styles from "./PetListaCard.module.css";
+
 const PetListaCard = ({ pet: p, showInfo }) => {
   return (
-    <div className={styles.miniCard}>
-      <div className={styles.suporte}>
-        <div className={styles.male}>
+    <div className={styles.cardInfo}>
+      <div className={styles.petInfoContainer}>
+        <div>
           <img
             className={styles.imagem_raca}
-            src={p.sexo == "M" ? male : female}
-            alt=""
+            src={p.sexo === "M" ? male : female}
+            alt="Sexo do pet"
           />
         </div>
-        <div className={styles.subInfo}>
-          <span className={styles.estiloNome}>{p.nome}</span>
-          <span className={styles.estiloEspecie}>{p.especie}</span>
-          <span className={styles.estiloDono}>{p.dono}</span>
+        <div>
+          <span className={styles.nomeCliente}>{p.nome}</span>
+          <div className={styles.layoutInfoPerson}>
+            <span>{p.especie}</span>
+            <span>|</span>
+            <span>{p.dono}</span>
+          </div>
         </div>
       </div>
-      <div>
-        <button onClick={() => showInfo()} className={styles.bttn}>
-          <img src={seta} alt="seta" />
-        </button>
+
+      <div className={styles.position}>
+        <div className={styles.position2}>
+          <div className={styles.alinhamentoImage}>
+            <img
+              src={iconEditar}
+              alt="Editar"
+              onClick={() => console.log("Editar pet")}
+            />
+          </div>
+          <div className={styles.alinhamentoImage}>
+            <img
+              src={iconDeletar}
+              alt="Deletar"
+              onClick={() => {
+                if (confirm("Deseja realmente deletar este pet?")) {
+                  console.log("Deletar pet");
+                }
+              }}
+            />
+          </div>
+        </div>
+        <div>
+          <img
+            src={seta}
+            alt="Mostrar detalhes"
+            onClick={() => showInfo(p)}
+          />
+        </div>
       </div>
     </div>
   );
