3e4949b163e90d78809221b043dc5f4cbe445814
Matheus <matheus.schmidt@aluno.senai.br>
2025-06-24 15:53:26 -0300

    tela client

--- PetAgenda-Projeto/src/pages/Clientes/Cadastrar_Clientes.jsx
+++ PetAgenda-Projeto/src/pages/Clientes/Cadastrar_Clientes.jsx
@@ -1,18 +1,18 @@
-import styles from "./Cadastrar_Clientes.module.css"
-import { useForm } from "react-hook-form"
-import { useEffect,useState } from "react";
+import styles from "./Cadastrar_Clientes.module.css";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 import { clientes } from "../../data/testeDeTabela";
-import { useAuth } from '../../contexts/UserContext';
+import { useAuth } from "../../contexts/UserContext";
 import CamposEndereco from "../../components/Endereco/CamposEndereco";
 import { useNavigate } from "react-router-dom";
-
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
 const CadastrarClientes = () => {
   const { empresaFetch, validar } = useAuth();
-  const [ servicos, setServicos] = useState([]);
-  const [ endereco, setEndereco ] = useState({});
-  const [ cep, setCep ] = useState("");
+  const [servicos, setServicos] = useState([]);
+  const [endereco, setEndereco] = useState({});
+  const [cep, setCep] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [servicosSelecionados, setServicosSelecionados] = useState([]);
 
   const {
@@ -20,11 +20,11 @@ const CadastrarClientes = () => {
     setValue,
     getValues,
     handleSubmit,
-    formState:{errors},
+    formState: { errors },
     subscribe,
     reset,
-    watch
-} = useForm();
+    watch,
+  } = useForm();
 
   async function preencherEnderecosCEP(cep) {
     try {
@@ -46,9 +46,9 @@ const CadastrarClientes = () => {
         "siafi": "5625"
       }
       */
-  
+
       if (endRes.status == 200) {
-        console.log('fetch cep');
+        console.log("fetch cep");
 
         const jsonBody = await endRes.json();
         if (!jsonBody.erro) {
@@ -58,16 +58,18 @@ const CadastrarClientes = () => {
             numero: "",
             cidade: jsonBody.localidade,
             estado: jsonBody.uf,
-          }
-  
+          };
+
           const entries = Object.entries(endFound);
-  
+
           for (const [key, value] of entries) {
             setValue(`endereco.${key}`, value, { shouldTouch: true });
           }
         }
       } else {
-        throw new Error("Falha ao obter informaçoes de endereço a partir de CEP");
+        throw new Error(
+          "Falha ao obter informaçoes de endereço a partir de CEP"
+        );
       }
     } catch (err) {
       console.error(err.message);
@@ -82,60 +84,61 @@ const CadastrarClientes = () => {
   }, [cep]);
 
   function handleEnderecoChange(e) {
-    const newEnd = {...endereco };
-    newEnd[e.target.name.split('.')[1]] = e.target.value;
-    
+    const newEnd = { ...endereco };
+    newEnd[e.target.name.split(".")[1]] = e.target.value;
+
     setEndereco(newEnd);
-    setValue(e.target.name.split('.')[1], e.target.value);
+    setValue(e.target.name.split(".")[1], e.target.value);
   }
-  
+
   // Pego os serviços oferecidos do banco da empresa
   useEffect(() => {
     if (validar) {
-      empresaFetch('/servico-oferecido')
-      .then(res => res.json())
-      .then(data => {
+      empresaFetch("/servico-oferecido")
+        .then((res) => res.json())
+        .then((data) => {
           setServicos(data.servicosOferecidos);
-      })
-      .catch(error => {
+        })
+        .catch((error) => {
           console.error("Erro ao buscar serviçoes oferecidos:", error);
-      });
+        });
     }
   }, []);
 
-  // Aqui esta pegando o serviço pelo id 
+  // Aqui esta pegando o serviço pelo id
   const handleSelectChange = (e) => {
     if (servicos?.length > 0) {
       const selectedId = e.target.value;
       const servico = servicos.find((s) => s.id.toString() === selectedId);
-  
-      if (!servicosSelecionados.some(s => s.id === servico.id)) {
-        setServicosSelecionados(prev => [...prev, servico]);
-      }
 
+      if (!servicosSelecionados.some((s) => s.id === servico.id)) {
+        setServicosSelecionados((prev) => [...prev, servico]);
+      }
     }
   };
 
   async function cadastrarCliente(objCli) {
-    return empresaFetch('/cliente', { method: "POST", body: JSON.stringify(objCli) })
-    .then( async res => { 
-        if (res.status == 200) {
-            const json = await res.json()
+    return empresaFetch("/cliente", {
+      method: "POST",
+      body: JSON.stringify(objCli),
+    }).then(async (res) => {
+      if (res.status == 200) {
+        const json = await res.json();
 
-            return json;
-        } else {
-            alert('erro ao cadastrar cliente');
-        }
+        return json;
+      } else {
+        alert("erro ao cadastrar cliente");
+      }
     });
   }
 
   const onSubmit = async (data) => {
-    console.log('submit: ', data)
+    console.log("submit: ", data);
     const objCli = {
       ...data,
       servico: undefined,
-      servicoRequerido: servicosSelecionados.map( s => ({ servico: s.id })),
-    }
+      servicoRequerido: servicosSelecionados.map((s) => ({ servico: s.id })),
+    };
 
     if (validar) {
       const jsonResp = await cadastrarCliente(objCli);
@@ -147,147 +150,153 @@ const CadastrarClientes = () => {
         navigate(`/empresa/clientes/lista`);
       }
     }
-  }
+  };
 
   const onError = (errors) => {
-    console.log('error ao enviar: ', errors)
-  }
+    console.log("error ao enviar: ", errors);
+  };
 
   return (
-    <div>
-      <div className={styles.tituloPage}>
-        <h1>Cadastro de Cliente</h1>
-        <hr />
-      </div>
+    <div className="containergeral mt-1">
+      <h1 className="cadastrar_agendamento__title">Novo Cliente</h1>
+      <hr />
+      <Container className="cadatrar_agendamento mt-4">
+        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+          <Row>
+            <Col className="campos-espaco">
+              <Form.Group controlId="formNome">
+                <Form.Label>Nome</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Digite o nome"
+                  isInvalid={!!errors.nome}
+                  {...register("nome", {
+                    required: "O nome é obrigatório",
+                    minLength: {
+                      value: 10,
+                      message: "O nome deve ter pelo menos 10 caracteres",
+                    },
+                    maxLength: {
+                      value: 100,
+                      message: "O nome deve ter no máximo 100 caracteres",
+                    },
+                    onChange: (e) => {
+                      let value = e.target.value;
+                      value = value
+                        .split(" ")
+                        .map((v) => v.charAt(0).toUpperCase() + v.slice(1))
+                        .join(" ");
+                      setValue("nome", value);
+                    },
+                  })}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.nome?.message}
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Col>
 
-      <div className={styles.areaCadastro}>
+            <Col>
+              <Form.Group controlId="formTelefone">
+                <Form.Label>Telefone</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="27 99988-7766"
+                  isInvalid={!!errors.telefone}
+                  {...register("telefone", {
+                    required: "O telefone é obrigatório",
+                    pattern: {
+                      value: /^\d{2} \d{5}-\d{4}$/,
+                      message: "Formato esperado: 27 99888-7766",
+                    },
+                    onChange: (e) => {
+                      let value = e.target.value.replace(/\D/g, "");
+                      value = `${value.slice(0, 2)}${
+                        value.length > 2 ? " " : ""
+                      }${value.slice(2, 7)}${
+                        value.length > 7 ? "-" : ""
+                      }${value.slice(7, 11)}`;
+                      setValue("telefone", value);
+                    },
+                  })}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.telefone?.message}
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+          </Row>
 
-        <form action="" onSubmit={handleSubmit(onSubmit, onError)} className={styles.estiloForm}>
-          {/* Aqui é a área de nome e telefone */}
-          <div className={styles.infoNomeTelefone}>
-
-            <div className={styles.estiloCampos}>
-              <label htmlFor="">Nome</label>
-              <input 
-                className={styles.nome} 
-                placeholder="Digite o nome" 
-                type="text" 
-                {...register("nome", {
-                required:"O nome é obrigatorio",
-                onChange: (e) => {
-                  let value = e.target.value;
-
-                  if (value && value.length > 0) {
-                    let values = value.split(' ');
-                    values = values.map( v => `${v.charAt(0).toUpperCase()}${v.substring(1)}` );
-                    value = values.join(' ');
-                  }
-                  setValue(e.target.name, value);
-                },
-                minLength:{
-                    value:10,
-                    message:"O nome deve ter pelo menos 15 caracteres"
-                },
-                maxLength:{
-                    value:80,
-                    message:"O nome dever ter no maximo 100 caracteres"
-                }
-              })}/>
-              {errors.nome && <p style={{color: 'red'}}>{errors.nome.message}</p>}
-            </div>
-
-            <div className={styles.estiloCampos}>
-              <label htmlFor="">Telefone</label>
-              <input type="text" placeholder="27 99988-7766" {...register("telefone", {
-                required:"O telefone é obrigatorio",
-                pattern: {
-                  value: /^\d{2,2} \d{5,5}-\d{4,4}$/,
-                  message: "Formato esperado: 27 99888-7766",
-                },
-                onChange: (e) => {
-                  let value = e.target.value;
-
-                  if (value && value.length > 0) {
-                    value = value.replaceAll(/[^0-9]/g, '');
-                    value = `${value.substring(0,2)}${(value.length > 2) ? " " : "" }${value.substring(2,7)}${(value.length > 7) ? "-" : "" }${value.substring(7,11)}`;
-                    console.log('limpei')
-                  }
-                  setValue(e.target.name, value);
-                }
-              })}/>
-              {errors.telefone && <p style={{color: 'red'}}>{errors.telefone.message}</p>}
-            </div>
-
-          </div>
-
-          {/* Aqui é a área de endereço */}
-          <div>
-            <div className={styles.estiloTitulos}>
+          <Row className="mt-4">
+            <Col>
               <h3>Endereço</h3>
-            </div>
-            <hr />
-            <CamposEndereco 
-              setValue={setValue}
-              cep={cep}
-              setCep={setCep}
-              endereco={endereco}
-              handleChange={handleEnderecoChange}
-              register={register} 
-              errors={errors}/>
-          </div>
-          
-          {/* Aqui é a área de adicionar serviço requeridos */}
-          <div>
+              <hr />
+              <CamposEndereco
+                setValue={setValue}
+                cep={cep}
+                setCep={setCep}
+                endereco={endereco}
+                handleChange={handleEnderecoChange}
+                register={register}
+                errors={errors}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
               <h3>Serviços Requeridos</h3>
               <hr />
-              <div>
-                <div className={styles.estiloCampos}>
-                  <label htmlFor="">Serviços</label>
+              <Form.Group controlId="formServico">
+                <Form.Label>Serviço</Form.Label>
+                <Form.Select
+                  {...register("servico")}
+                  onChange={handleSelectChange}
+                >
+                  <option value="">Selecione um serviço</option>
+                  {servicos.map((servico) => (
+                    <option key={servico.id} value={servico.id}>
+                      {servico.nome}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Form.Group>
 
-                    <select {...register("servico", { required: false })} id="servico" onChange={handleSelectChange}>
-
-                        <option value="">Selecione um serviço</option>
-
-                        {servicos?.length > 0 && servicos.map((servico) => (
-                            <option key={servico.id} value={servico.id}>{servico.nome}</option>
-                        ))}
-
-                    </select>
-
-                </div>
-
-                {/* Aqui vai ficar a logica de preencher a tabela com o nome e categoria do serviço */}
-                <div className={styles.tabelaServicos}>
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Nome</th>
-                        <th>Categoria</th>
+              <div className="mt-3">
+                <table className="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Nome</th>
+                      <th>Categoria</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {servicosSelecionados.map((servico) => (
+                      <tr key={servico.id}>
+                        <td>{servico.nome}</td>
+                        <td>{servico.nomeCategoria}</td>
                       </tr>
-                    </thead>
-                    <tbody>
-                      {/* No lugar de clientes, eu coloco o objeto serviços que irá ser retornado do banco */}
-                      {servicosSelecionados.map((servico) => (
-                        <tr key={servico.id}>
-                          <td>{servico.nome}</td>
-                          <td>{servico.nomeCategoria}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
+                    ))}
+                  </tbody>
+                </table>
               </div>
-          </div>
+            </Col>
+          </Row>
 
-          <div className={styles.customizeBotaoCadastro}>
-            <button type="submit">Cadastrar</button>
-          </div>
-        </form>
-
-      </div>
-
+          <Row className="d-flex justify-content-center mt-4">
+            <Col md="auto">
+              <Button
+                variant="primary"
+                type="submit"
+                className="botao__cadastrar"
+              >
+                Cadastrar
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default CadastrarClientes
+export default CadastrarClientes;
